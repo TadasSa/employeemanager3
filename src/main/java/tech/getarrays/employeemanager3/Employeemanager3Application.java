bfc6c904c6fc82cd0678b7c7304b03b9823421fe
TadasSa <t.sabaitis@gmail.com>
2021-07-30 21:01:46 +0300

    Issprendziau buvusi error. Beda buvo kad per ilgas path kompe kur projektas buvo. Prideta CORS config (yra error) -> pavyko isspresti :)

--- src/main/java/tech/getarrays/employeemanager3/Employeemanager3Application.java
+++ src/main/java/tech/getarrays/employeemanager3/Employeemanager3Application.java
@@ -1,6 +1,7 @@
 package tech.getarrays.employeemanager3;
 
-import org.apache.catalina.filters.CorsFilter;
+//import org.apache.catalina.filters.CorsFilter;   --->>>
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -21,15 +22,16 @@ public class Employeemanager3Application {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control_request-Headers"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
 				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 		return new CorsFilter(urlBasedCorsConfigurationSource);
-		//test
 	}
 }
